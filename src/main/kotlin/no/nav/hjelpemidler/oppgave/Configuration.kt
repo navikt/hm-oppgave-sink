3b4d8f2f510db7e3ed062833aec716f4e49b30e2
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-04-06 15:39:41 +0200

    Attempting to fix Azure problems by using new naming scheme for azure scopes

--- src/main/kotlin/no/nav/hjelpemidler/oppgave/Configuration.kt
+++ src/main/kotlin/no/nav/hjelpemidler/oppgave/Configuration.kt
@@ -40,7 +40,7 @@ private val devProperties = ConfigurationMap(
         "pdf.baseurl" to "http://hm-soknad-pdfgen.teamdigihot.svc.cluster.local",
         "AZURE_TENANT_BASEURL" to "https://login.microsoftonline.com",
         "oppgave.baseurl" to "https://digihot-proxy.dev-fss-pub.nais.io/oppgave-aad",
-        "PROXY_SCOPE" to "api://9a0d62f4-00cf-462a-9acc-0e76e7a360ae/.default",
+        "PROXY_SCOPE" to "api://dev-fss.teamdigihot.digihot-proxy/defaultaccess",
         "pdl.baseurl" to "https://digihot-proxy.dev-fss-pub.nais.io/pdl-aad",
     )
 )
