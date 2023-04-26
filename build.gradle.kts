63c620bfc425a72ca52446b16856fdf4542470da
Erik Eiesland <erik@eies.land>
2023-04-26 07:47:37 +0200

    gradle 8.1.1, fjerner buildSrc, oppgraderer versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,76 +1,56 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version Kotlin.version
-    id(Spotless.spotless) version Spotless.version
-    id(Shadow.shadow) version Shadow.version
-}
-
-buildscript {
-    repositories {
-        jcenter()
-    }
-}
-
-apply {
-    plugin(Spotless.spotless)
+    kotlin("jvm") version "1.8.20"
+    id("com.diffplug.spotless") version "6.18.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
     mavenCentral()
-    jcenter()
     maven("https://packages.confluent.io/maven/")
     maven("https://jitpack.io")
 }
 
 application {
     applicationName = "hm-oppgave-sink"
-    mainClassName = "no.nav.hjelpemidler.oppgave.ApplicationKt"
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    // mainClassName = "no.nav.hjelpemidler.oppgave.ApplicationKt"
 }
 
 dependencies {
-    implementation(Jackson.core)
-    implementation(Jackson.kotlin)
-    implementation(Jackson.jsr310)
-    implementation("com.github.guepardoapps:kulid:1.1.2.0")
-    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70")
-    implementation("org.slf4j:slf4j-api:2.0.0-alpha6") // fordi rapids-and-rivers er på logback-classic:1.3.0-alpha10 som krever slf4j >= 2.0.0-alpha4
-    implementation("io.github.microutils:kotlin-logging:2.1.21")
-    implementation(Ktor.serverNetty)
-    implementation(Database.Kotlinquery)
-    implementation(Fuel.fuel)
-    implementation(Fuel.library("coroutines"))
-    implementation(Konfig.konfig)
-    implementation(Kotlin.Logging.kotlinLogging)
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib"))
+    implementation("com.github.navikt:rapids-and-rivers:2023041310341681374880.67ced5ad4dda")
 
-    testImplementation(Junit5.api)
-    testImplementation(KoTest.assertions)
-    testImplementation(KoTest.runner)
-    testImplementation(Mockk.mockk)
-    implementation(Wiremock.standalone)
-    testImplementation(Junit5.engine)
+    // Logging
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
+
+    // Fuel fixme -> bytt med hm-http
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.3.1")
+
+    // fixme -> bytt med hm-http
+    implementation("com.natpryce:konfig:1.6.10.0")
+
+    // fixme -> bytt med MockEngine
+    implementation("com.github.tomakehurst:wiremock:2.27.2")
+
     testImplementation(kotlin("test"))
 }
 
 spotless {
     kotlin {
-        ktlint(Ktlint.version)
+        ktlint()
     }
     kotlinGradle {
-        target("*.gradle.kts", "buildSrc/*.gradle.kts")
-        ktlint(Ktlint.version)
+        target("*.gradle.kts")
+        ktlint()
     }
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
 
@@ -82,16 +62,4 @@ tasks.withType<Test> {
         exceptionFormat = TestExceptionFormat.FULL
         events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
     }
-}
-
-tasks.named("shadowJar") {
-    dependsOn("test")
-}
-
-tasks.named("jar") {
-    dependsOn("test")
-}
-
-tasks.named("compileKotlin") {
-    dependsOn("spotlessCheck")
 }
