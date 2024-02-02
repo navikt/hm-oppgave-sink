3eae3e24f95755e4842945f12541eb1570920794
Ole Steinar L. Skrede <ole.steinar.lillestol.skrede@nav.no>
2024-02-02 09:47:34 +0100

    skip eventid 6b8d59cd-3b7c-48a4-86bc-fea690758f85

--- src/main/kotlin/no/nav/hjelpemidler/oppgave/service/RutingOppgaveSink.kt
+++ src/main/kotlin/no/nav/hjelpemidler/oppgave/service/RutingOppgaveSink.kt
@@ -118,6 +118,7 @@ internal class RutingOppgaveSink(
             setOf(
                 UUID.fromString("47376212-4289-4c0c-b6e6-417e4c989193"),
                 UUID.fromString("d0a68746-4804-44f1-931f-0b52ec9f9c95"),
+                UUID.fromString("6b8d59cd-3b7c-48a4-86bc-fea690758f85"),
             )
         return eventId in skipList
     }
