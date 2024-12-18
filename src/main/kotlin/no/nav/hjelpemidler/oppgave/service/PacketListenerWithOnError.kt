5497866ba7db8e2e029b4e8e1ac43284fdcc6a7d
Erik Eiesland <erik.eiesland@nav.no>
2024-12-18 14:11:36 +0100

    pynter på logging

--- src/main/kotlin/no/nav/hjelpemidler/oppgave/service/PacketListenerWithOnError.kt
+++ src/main/kotlin/no/nav/hjelpemidler/oppgave/service/PacketListenerWithOnError.kt
@@ -4,11 +4,16 @@ import com.github.navikt.tbd_libs.rapids_and_rivers.River
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageProblems
+import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.hjelpemidler.logging.secureLog
+
+private val log = KotlinLogging.logger {}
 
 interface PacketListenerWithOnError : River.PacketListener {
     override fun onError(problems: MessageProblems, context: MessageContext, metadata: MessageMetadata) {
-        secureLog.info { "Feil i melding fra rapid: '${problems.toExtendedReport()}'" }
-        error("Feil i melding fra rapid, se secureLog for detaljer")
+        val message = "Validering av melding feilet, se secureLog for detaljer"
+        log.info { message }
+        secureLog.info { "Validering av melding feilet: '${problems.toExtendedReport()}'" }
+        error(message)
     }
 }
