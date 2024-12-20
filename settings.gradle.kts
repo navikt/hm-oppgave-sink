12e748cae000637691de4e82cbeeba8cf4031d99
Erik Eiesland <erik.eiesland@nav.no>
2024-12-20 13:41:31 +0100

    hotlibs/katalog -> 24.355.130518 med nyere rapids-and-rivers

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:katalog:24.352.170401")
+            from("no.nav.hjelpemidler:katalog:24.355.130518")
         }
     }
 }
